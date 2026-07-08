59376f512407708407636641c5b8dc8bbddaa4f7
aa <aa>
2026-07-08 11:12:09 +0900

    소켓통신 진행중

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
-import { getUserGreeting } from "./users"; // 작성한 함수 임포트
+import userRouter from "./users/router"; // 작성한 함수 임포트
 
 const app = express();
+app.use(express.json());
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: { origin: "*" }, // 프론트엔드 연결 허용
@@ -17,18 +18,7 @@ io.on("connection", (socket) => {
   });
 });
 
-//user쪽
-app.post("/user/:name", (req, res) => {
-  const { name } = req.params;
-  const result = getUserGreeting(name);
-  res.json(result);
-});
-// API 엔드포인트
-app.get("/user/:name", (req, res) => {
-  const { name } = req.params;
-  const result = getUserGreeting(name);
-  res.json(result);
-});
+app.use("/user", userRouter);
 
 httpServer.listen(3000, () => {
   console.log("채팅 서버가 http://localhost:3000 에서 실행 중입니다.");
---
+++ src/users/router.ts
@@ -0,0 +1,66 @@
+import { Router } from "express";
+
+const router = Router();
+
+// [인증 관련]
+router.post("/login", (req, res) => {
+  // 1. req.body에서 아이디/비번 추출
+  // 2. 유저 테이블 조회 (DB)
+  // 3. 비밀번호 검증
+  // 4. 세션 생성 또는 JWT 토큰 발행
+  res.json({ message: "Login successful" });
+});
+router.get("/getUserList", (req, res) => {
+  res.json([
+    {
+      이름: "abc",
+      부서: "파트",
+      직책: "직책",
+      상태: "접속중",
+    },
+    {
+      이름: "abc",
+      부서: "파트",
+      직책: "직책",
+      상태: "접속중",
+    },
+    {
+      이름: "abc",
+      부서: "파트",
+      직책: "직책",
+      상태: "접속중",
+    },
+  ]);
+});
+
+// //유저 상세 조회
+// export const getUserDetail = (userId: string) => {
+//   return {
+//     이름: "abc",
+//     연락처: "010-0000-0000",
+//     부서: "파트",
+//     직책: "직책",
+//     상태: "휴가중",
+//   };
+// };
+
+// //로그인
+// export const login = (userId: string, password: string) => {
+//   //세션담기
+// };
+
+// interface User {
+//   userId: string;
+//   password: string;
+//   phone: string;
+//   dept?: string;
+//   roles?: string;
+// }
+
+// //회원가입
+// export const signUp = (user: User) => {};
+
+// //회원탈퇴
+// export const delUser = (userId: string, password: string) => {};
+
+export default router; // 반드시 이게 있어야 합니다!
